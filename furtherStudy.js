744309287c5c2bc64cf973789d57aaefebf7a8d1
Wooyang Son <sonwy102@gmail.com>
2020-10-21 16:15:03 +0000

    Added further study

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,32 +1,48 @@
 "use strict";
 
-// 1. isHometown
+function wordsInCommon(words1, words2) {
+  // Replace this with your code
+  const words1Set = new Set(words1);
+  const words2Set = new Set(words2);
+  const results = new Set();
 
-function isHometown(town) {
-  return town === "San Francisco";
+  for (const word of words1Set) {
+    if (words2Set.has(word)) {
+      results.add(word);
+    }
+  }
+  const resultArray = Array.from(results);
+  return resultArray;
 }
 
-// 2. getFullName
-function getFullName(firstName, lastName) {
-  return `${firstName} ${lastName}`;
-}
+function kidsGame(names) {
+  const output = [names.shift()];
 
-// 3. calculateTotal
-function calculateTotal(basePrice, state, tax = 0.05) {
-  let subtotal = basePrice * (1 + tax);
+  const firstLetterToWords = {};
 
-  let fee = 0;
-  if (state === "CA") {
-    fee = 0.03 * subtotal;
-  } else if (state === "PA") {
-    fee = 2;
-  } else if (state === "MA") {
-    if (basePrice <= 100) {
-      fee = 1;
+  for (const name of names) {
+    if (firstLetterToWords[name[0]] === undefined) {
+      firstLetterToWords[name[0]] = [name];
     } else {
-      fee = 3;
+      firstLetterToWords[name[0]].push(name);
     }
   }
 
-  return subtotal + fee;
+  while (true) {
+    const lastWord = output[output.length - 1];
+    console.log(lastWord);
+    const startLetter = lastWord[lastWord.length - 1];
+    console.log(startLetter);
+
+    console.log(firstLetterToWords);
+    if (firstLetterToWords[startLetter] === []) {
+      break;
+    }
+
+    const word = firstLetterToWords[startLetter].shift();
+    output.push(word);
+  }
+
+  return output;
 }
+
